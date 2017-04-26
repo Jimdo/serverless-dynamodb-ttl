e0430c14a8d48f3e88d8df1b441e75b61278df1e
sbstjn <mail@sbstjn.com>
2017-04-26 14:59:35 +0200

    Add tests

--- src/plugin.js
+++ src/plugin.js
@@ -1,8 +1,64 @@
 'use strict'
 
+const AWS = require('aws-sdk')
+const util = require('util')
+
 class Plugin {
   constructor (serverless, options) {
     this.serverless = serverless
+    this.options = options || {}
+
+    this.dynamodb = new AWS.DynamoDB({ region: this.options.region })
+
+    this.hooks = {
+      'after:deploy:deploy': this.afterDeploy.bind(this)
+    }
+  }
+
+  afterDeploy () {
+    if (typeof this.options.noDeploy !== 'undefined' && this.options.noDeploy) {
+      return Promise.resolve()
+    }
+
+    if (this.serverless.cli) { 
+      this.serverless.cli.log('Enabling TTL setting(s) for DynamoDB')
+    }
+
+    return Promise.all(
+      this.tables.map(
+        data => this.check(data.table).then(
+          enabled => enabled || this.enable(data)
+        )
+      )
+    ).catch(
+      error => this.serverless.cli && this.serverless.cli.log(util.format('Failed to set TTL for DynamoDB: %s', error))
+    )
+  }
+
+  check (table) {
+    return this.dynamodb.describeTimeToLive(
+      {
+        TableName: table
+      }
+    ).promise().then(
+      res => res.TimeToLiveDescription.TimeToLiveStatus === 'ENABLED'
+    )
+  }
+
+  enable (data) {
+    return this.dynamodb.updateTimeToLive(
+      {
+        TableName: data.table,
+        TimeToLiveSpecification: {
+          AttributeName: data.field,
+          Enabled: true
+        }
+      }
+    ).promise()
+  }
+
+  get tables () {
+    return this.serverless.service.custom.dynamodb.ttl
   }
 }
 
--- test/plugin.spec.js
+++ test/plugin.spec.js
@@ -1,9 +1,204 @@
 'use strict'
 
+var AWS = require('aws-sdk-mock');
 const Plugin = require('../')
 
-it('works', () => {
-  const p = new Plugin({foo: 'bar'}, {})
+describe('Plugin', () => {
+  let dynamoDBdescribeTimeToLiveSpy
+  let dynamoDBupdateTimeToLiveSpy
 
-  expect(p.serverless).toHaveProperty('foo', 'bar')
+  afterEach(() => {
+    AWS.restore('DynamoDB');
+  })
+
+  it('reads configuration', () => {
+    const config = {
+      service: {
+        custom: {
+          dynamodb: {
+            ttl: [
+              {
+                table: 'my-table-1',
+                field: 'my-field-1'
+              },
+              {
+                table: 'my-table-2',
+                field: 'my-field-2'
+              }
+            ]
+          }
+        }
+      }
+    }
+
+    const test = new Plugin(config)
+
+    expect(test.tables).toContainEqual({ table: 'my-table-1', field: 'my-field-1' })
+    expect(test.tables).toContainEqual({ table: 'my-table-2', field: 'my-field-2' })
+  })
+
+  it('Updates TTL setting if not alreadt set', () => {
+    let dynamoDBdescribeTimeToLiveSpy = jest.fn((_, cb) => cb(null, { TimeToLiveDescription: { TimeToLiveStatus: false } }))
+    let dynamoDBupdateTimeToLiveSpy = jest.fn((_, cb) => cb(null, ''))
+
+    AWS.mock('DynamoDB', 'describeTimeToLive', dynamoDBdescribeTimeToLiveSpy)
+    AWS.mock('DynamoDB', 'updateTimeToLive', dynamoDBupdateTimeToLiveSpy)
+
+    const config = {
+      service: {
+        custom: {
+          dynamodb: {
+            ttl: [
+              {
+                table: 'my-table-1',
+                field: 'my-field-1'
+              }
+            ]
+          }
+        }
+      }
+    }
+
+    return new Plugin(config, { region: 'eu-west-1' }).afterDeploy().then(
+      () => {
+        expect(dynamoDBdescribeTimeToLiveSpy).toHaveBeenCalledTimes(1)
+        expect(dynamoDBupdateTimeToLiveSpy).toHaveBeenCalledTimes(1)
+      }
+    )
+  })
+
+  it('Does not update TTL configuration if already set', () => {
+    let dynamoDBdescribeTimeToLiveSpy = jest.fn((_, cb) => cb(null, { TimeToLiveDescription: { TimeToLiveStatus: 'ENABLED' } }))
+    let dynamoDBupdateTimeToLiveSpy = jest.fn((_, cb) => cb(null, ''))
+
+    AWS.mock('DynamoDB', 'describeTimeToLive', dynamoDBdescribeTimeToLiveSpy)
+    AWS.mock('DynamoDB', 'updateTimeToLive', dynamoDBupdateTimeToLiveSpy)
+
+    const config = {
+      service: {
+        custom: {
+          dynamodb: {
+            ttl: [
+              {
+                table: 'my-table-1',
+                field: 'my-field-1'
+              }
+            ]
+          }
+        }
+      }
+    }
+
+    return new Plugin(config, { region: 'eu-west-1' }).afterDeploy().then(
+      () => {
+        expect(dynamoDBdescribeTimeToLiveSpy).toHaveBeenCalledTimes(1)
+        expect(dynamoDBupdateTimeToLiveSpy).toHaveBeenCalledTimes(0)
+      }
+    )
+  })
+
+  it('Does work for multiple table configuration #1', () => {
+    let dynamoDBdescribeTimeToLiveSpy = jest.fn((_, cb) => cb(null, { TimeToLiveDescription: { TimeToLiveStatus: 'ENABLED' } }))
+    let dynamoDBupdateTimeToLiveSpy = jest.fn((_, cb) => cb(null, ''))
+
+    AWS.mock('DynamoDB', 'describeTimeToLive', dynamoDBdescribeTimeToLiveSpy)
+    AWS.mock('DynamoDB', 'updateTimeToLive', dynamoDBupdateTimeToLiveSpy)
+
+    const config = {
+      service: {
+        custom: {
+          dynamodb: {
+            ttl: [
+              {
+                table: 'my-table-1',
+                field: 'my-field-1'
+              },
+              {
+                table: 'my-table-1',
+                field: 'my-field-1'
+              }
+            ]
+          }
+        }
+      }
+    }
+
+    return new Plugin(config, { region: 'eu-west-1' }).afterDeploy().then(
+      () => {
+        expect(dynamoDBdescribeTimeToLiveSpy).toHaveBeenCalledTimes(2)
+        expect(dynamoDBupdateTimeToLiveSpy).toHaveBeenCalledTimes(0)
+      }
+    )
+  })
+
+  it('Does work for multiple table configuration #2', () => {
+    let dynamoDBdescribeTimeToLiveSpy = jest.fn((_, cb) => cb(null, { TimeToLiveDescription: { TimeToLiveStatus: false } }))
+    let dynamoDBupdateTimeToLiveSpy = jest.fn((_, cb) => cb(null, ''))
+
+    AWS.mock('DynamoDB', 'describeTimeToLive', dynamoDBdescribeTimeToLiveSpy)
+    AWS.mock('DynamoDB', 'updateTimeToLive', dynamoDBupdateTimeToLiveSpy)
+
+    const config = {
+      service: {
+        custom: {
+          dynamodb: {
+            ttl: [
+              {
+                table: 'my-table-1',
+                field: 'my-field-1'
+              },
+              {
+                table: 'my-table-1',
+                field: 'my-field-1'
+              }
+            ]
+          }
+        }
+      }
+    }
+
+    return new Plugin(config, { region: 'eu-west-1' }).afterDeploy().then(
+      () => {
+        expect(dynamoDBdescribeTimeToLiveSpy).toHaveBeenCalledTimes(2)
+        expect(dynamoDBupdateTimeToLiveSpy).toHaveBeenCalledTimes(2)
+      }
+    )
+  })
+
+  it('Does work for multiple table configuration #3', () => {
+    let counter = 0
+
+    let dynamoDBdescribeTimeToLiveSpy = jest.fn((_, cb) => cb(null, { TimeToLiveDescription: { TimeToLiveStatus: counter++ == 1 ? 'ENABLED' : false } }))
+    let dynamoDBupdateTimeToLiveSpy = jest.fn((_, cb) => cb(null, ''))
+
+    AWS.mock('DynamoDB', 'describeTimeToLive', dynamoDBdescribeTimeToLiveSpy)
+    AWS.mock('DynamoDB', 'updateTimeToLive', dynamoDBupdateTimeToLiveSpy)
+
+    const config = {
+      service: {
+        custom: {
+          dynamodb: {
+            ttl: [
+              {
+                table: 'my-table-1',
+                field: 'my-field-1'
+              },
+              {
+                table: 'my-table-1',
+                field: 'my-field-1'
+              }
+            ]
+          }
+        }
+      }
+    }
+
+    return new Plugin(config, { region: 'eu-west-1' }).afterDeploy().then(
+      () => {
+        expect(dynamoDBdescribeTimeToLiveSpy).toHaveBeenCalledTimes(2)
+        expect(dynamoDBupdateTimeToLiveSpy).toHaveBeenCalledTimes(1)
+      }
+    )
+  })
+
 })
